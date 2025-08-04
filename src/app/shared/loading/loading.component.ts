b7442b36bf7102cc9e8ad66ab3c01b989a5fc176
cesarsebastiandev <cesarsebastian.dev@gmail.com>
2025-08-04 12:13:10 -0600

    add: spinner implementation

---
+++ src/app/shared/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgxUiLoaderModule } from "ngx-ui-loader";
+
+@Component({
+  selector: 'app-loading',
+  imports: [NgxUiLoaderModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+
+}
